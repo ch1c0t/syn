f1df27529ce4354cc014a5a9c613d0f6a47bba22
Anatoly Chernow <chertoly@gmail.com>
2017-09-02 22:08:36 +0300

    Add Literal::String.parse

--- spec/Literal/String_spec.rb
+++ spec/Literal/String_spec.rb
@@ -21,4 +21,17 @@ describe Syn::Literal::String do
       assert { not described_class.parsable? "[array]" }
     end
   end
+
+  describe '.parse' do
+    it 'can parse parsable' do
+      literal = described_class.parse "\"string\""
+      assert { literal.to_s == '"string"' }
+    end
+
+    it 'returns nil for unparsable' do
+      assert { described_class.parse("some_name").nil? }
+      assert { described_class.parse(":symbol").nil? }
+      assert { described_class.parse("[array]").nil? }
+    end
+  end
 end
--- src/Syn/Literal/String.class.rb
+++ src/Syn/Literal/String.class.rb
@@ -7,3 +7,11 @@ end
 def self.parsable? string
   string.start_with?('"') && string.end_with?('"')
 end
+
+def self.parse string
+  if parsable? string
+    literal = new
+    literal.string = string[1..-2]
+    literal
+  end
+end
